2adeb40c435ffa48c00a4c2c33139262d2eb389a
Mirko Franco <francomirko91@gmail.com>
2022-12-19 03:54:15 +0100

    ---------------------

---
+++ src/Start.java
@@ -0,0 +1,20 @@
+public class Start {
+    public static void main(String[] args) {
+
+
+        boolean A = !(!(!(false ^ false) || (true && true)));
+        System.out.println(A);
+
+
+
+        int x = 3;
+        int y = 2;
+        boolean z = !((x * y) <= 6) && (x - y != 1);
+        System.out.println(z);
+
+
+
+
+
+    }
+}
